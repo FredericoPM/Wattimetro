94266de8029a8e3d6748013ae39dbc531c82df74
FredericoPM <52333585+FredericoPM@users.noreply.github.com>
2021-04-06 15:03:25 -0300

    Start

---
+++ app/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Color(0xFF110A33),
+        accentColor: Color(0xFFFFD500),
+        errorColor: Color(0xFFDB5256),
+        canvasColor: Color(0xFF2D2843),
+        cardColor: Color(0xFF1E173B),
+        primaryTextTheme: Typography.material2018(platform: TargetPlatform.iOS).white,
+        textTheme: Typography.material2018(platform: TargetPlatform.iOS).white,
+        fontFamily: "Roboto Mono",
+      ),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Center(
+        child: Card(
+            child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$_counter',
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      )
+    );
+  }
+}
