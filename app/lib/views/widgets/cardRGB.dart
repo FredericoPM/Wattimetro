32fdda92c68dc5c04605a8433ba80391040b7f00
FredericoPM <52333585+FredericoPM@users.noreply.github.com>
2021-04-25 15:06:05 -0300

    conexão, recebimento e envio de data via MQTT

--- app/lib/views/widgets/cardRGB.dart
+++ app/lib/views/widgets/cardRGB.dart
@@ -1,3 +1,4 @@
+import 'package:app/controllers/conection.dart';
 import 'package:app/models/controle.dart';
 import 'package:app/views/widgets/cardTemplate.dart';
 import 'package:flutter/material.dart';
@@ -14,16 +15,29 @@ class CardRGB extends StatefulWidget {
 }
 
 class _CardRGBState extends State<CardRGB> {
+  ConnectionController conection;
   @override
   //! scroll do color picker conflitando com scroll do list view
   Widget build(BuildContext context) {
+    conection = ConnectionController(
+      broker: widget.controle.broker,
+      topic: widget.controle.topic,
+      onMensage: (String text){
+        print(text);
+      }
+    );
+    conection.startConnection();
     return CardTemplate(
       controle: widget.controle,
       controleDelete: widget.delete,
       controleUpdate: widget.update,
+
       childWidget: Center(
         child: CircleColorPicker(
           initialColor: Colors.blue,
+          onClick: (){
+            conection.publishMessage("teste");
+          },
           onChanged: (color){
             // setState(() {
             //   atualColor = color;
