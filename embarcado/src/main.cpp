5c78fbfbcee50e1e1d3660e4e18f89a0af277bf9
FredericoPM <52333585+FredericoPM@users.noreply.github.com>
2021-05-13 19:03:33 -0300

    FORMATAÇÃO

--- embarcado/src/main.cpp
+++ embarcado/src/main.cpp
@@ -1,5 +1,5 @@
-#include "EmonLib.h"   //https://github.com/openenergymonitor/EmonLib
 #include <WiFi.h>
+#include <EmonLib.h>
 #include <PubSubClient.h>
 
 #define PIN_CONNECTION 2
@@ -27,6 +27,27 @@ float kWh = 0;
 unsigned long lastmillis = millis();
 unsigned long lastmillisData = millis();
 
+//Reconecta-se ao WiFi
+void reconnectWiFi(void) {
+    //se já está conectado a rede WI-FI, nada é feito. 
+    //Caso contrário, são efetuadas tentativas de conexão
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+          
+    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
+      
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(100);
+        Serial.print(".");
+    }
+    
+    Serial.println();
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.print(SSID);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+}
 void initWiFi(void) {
     delay(10);
     Serial.println("------Conexao WI-FI------");
@@ -35,11 +56,6 @@ void initWiFi(void) {
     Serial.println("Aguarde");
       
     reconnectWiFi();
-}
-// Inicializa parâmetros de conexão MQTT(endereço do broker, porta e seta função de callback)
-void initMQTT(void) {
-    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
-    MQTT.setCallback(mqtt_callback);            //atribui função de callback
 }
 //Função de callback esta função é chamada toda vez que uma informação de um dos tópicos subescritos chega)
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
@@ -50,7 +66,8 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
        char c = (char)payload[i];
        msg += c;
     }
-
+    
+    Serial.println(msg);
 }
 //Função: Reconecta-se ao broker MQTT em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
 void reconnectMQTT(void) {
@@ -80,26 +97,10 @@ void VerificaConexoesWiFIEMQTT(void){
       
      reconnectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
 }
-//Reconecta-se ao WiFi
-void reconnectWiFi(void) {
-    //se já está conectado a rede WI-FI, nada é feito. 
-    //Caso contrário, são efetuadas tentativas de conexão
-    if (WiFi.status() == WL_CONNECTED)
-        return;
-          
-    WiFi.begin(SSID, PASSWORD); // Conecta na rede WI-FI
-      
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(100);
-        Serial.print(".");
-    }
-    
-    Serial.println();
-    Serial.print("Conectado com sucesso na rede ");
-    Serial.print(SSID);
-    Serial.println("IP obtido: ");
-    Serial.println(WiFi.localIP());
+// Inicializa parâmetros de conexão MQTT(endereço do broker, porta e seta função de callback)
+void initMQTT(void) {
+    MQTT.setServer(BROKER_MQTT, BROKER_PORT);   //informa qual broker e porta deve ser conectado
+    MQTT.setCallback(mqtt_callback);            //atribui função de callback
 }
 
 void readData() {
@@ -111,31 +112,37 @@ void readData() {
     amps = emon.Irms;
 }
 void sendData(){
-    char data[10];
+    char data[13];
+    kWh = ((int)kWh % 10000) + (kWh - (int)kWh);
     Serial.println(kWh);
     sprintf(data, "1|%f", kWh);
     MQTT.publish(TOPICO_PUBLISH, data);
 
+    voltage = ((int)voltage % 10000) + (voltage - (int)voltage);
     Serial.println(voltage);
     sprintf(data, "1|%f", voltage);
     MQTT.publish(TOPICO_PUBLISH, data);
 
+    amps = ((int)amps % 10000) + (amps - (int)amps);
     Serial.println(amps);
     sprintf(data, "1|%f", amps);
     MQTT.publish(TOPICO_PUBLISH, data);
 }
 void setup() {
-  Serial.begin(115200);
-  emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
-  emon.current(34, currCalibration); // Current: input pin, calibration.
-  initWiFi();
-  initMQTT(); 
+    Serial.begin(115200);
+    emon.voltage(35, vCalibration, 1.7); // Voltage: input pin, calibration, phase_shift
+    emon.current(34, currCalibration); 
+    pinMode(PIN_CONNECTION, OUTPUT);
+    digitalWrite(PIN_CONNECTION,LOW);// Current: input pin, calibration.
+    initWiFi();
+    initMQTT();
 }
 void loop() {
-    if(lastmillisData - millis() == 1000.0){
-        lastmillisData = millis();
-        sendData();
-    }
+    // if(lastmillisData - millis() == 1000.0){
+    //     lastmillisData = millis();
+    //     readData();
+    //     sendData();
+    // }
     VerificaConexoesWiFIEMQTT();
     MQTT.loop();
 }
